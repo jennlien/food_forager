8b3856b34093f4de0581fc1bf9169cd73e333b9e
Jenn Lien <jlien2014@kellogg.northwestern.edu>
2013-10-23 02:04:19 -0500

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodForager::Application.config.secret_key_base = '6e150f4251bcd67af6415afaad206848c97b59dbe36631d475d13550fec5540f9954652f987eb391aa29494d33009c62858439b89f48a6a297d5c685a65cec0a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FoodForager::Application.config.session_store :cookie_store, key: '_food_forager_session'
